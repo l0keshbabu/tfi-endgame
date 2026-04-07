b20de1f766460db1d8e95d9a558825892523be3e
l00kesh <kattalokeshbabuk@gmail.com>
2026-04-07 14:56:20 +0530

    feat: add heroes and movies data for TFI Endgame

---
+++ src/data/heroes.js
@@ -0,0 +1,47 @@
+export const heroes = [
+  {
+    name: "Prabhas",
+    backgroundColor: "#2E86C1",
+    color: "#fff"
+  },
+  {
+    name: "Mahesh Babu",
+    backgroundColor: "#28B463",
+    color: "#fff"
+  },
+  {
+    name: "Allu Arjun",
+    backgroundColor: "#8E44AD",
+    color: "#fff"
+  },
+  {
+    name: "Jr NTR",
+    backgroundColor: "#D35400",
+    color: "#fff"
+  },
+  {
+    name: "Ram Charan",
+    backgroundColor: "#C0392B",
+    color: "#fff"
+  },
+  {
+    name: "Pawan Kalyan",
+    backgroundColor: "#1F618D",
+    color: "#fff"
+  },
+  {
+    name: "Nani",
+    backgroundColor: "#117864",
+    color: "#fff"
+  },
+  {
+    name: "Vijay Deverakonda",
+    backgroundColor: "#7D3C98",
+    color: "#fff"
+  },
+  {
+  name: "Sampoornesh Babu",
+  backgroundColor: "#F1C40F",
+  color: "#000"
+}
+]
