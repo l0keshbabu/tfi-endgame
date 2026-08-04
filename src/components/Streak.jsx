1158ab20d18cd83c2f93a10e22507e107b8e533c
l00kesh <kattalokeshbabuk@gmail.com>
2026-08-04 16:06:54 +0530

    style: refine streak section layout and styling

--- src/components/Streak.jsx
+++ src/components/Streak.jsx
@@ -2,30 +2,38 @@ export default function Streak({currentStreak,bestStreak}){
     return( 
         <section className="streak-card">
             <div className="streak-box">
-            <p className="streak-title">🔥Box Office Run</p>
+            <p className="streak-title">🔥 Box Office Run</p>
             <h3 className="streak-text">
-                {currentStreak===0
-                    ? "No hit yet 🎭"
-                   :(
+  {currentStreak === 0 ? (
+    "First blockbuster awaits"
+  ) : (
     <>
-      <span className="gradient-text">
-        {currentStreak} Blockbusters in a row
+      <span className="streak-number gradient-text">
+        {currentStreak}
+      </span>
+
+      <span className="streak-label">
+        Blockbusters in a row
       </span>
     </>
   )}
-            </h3>
+</h3>
             </div>
             <div className="streak-box best-box">
                 <p className="streak-title">🏆 Hall of Fame</p>
                 <h3 className="streak-text">
                 {bestStreak===0
-                    ? "No hit yet 🎭"
+                    ? "No records yet "
                    :(
-    <>
-      <span className="best-gradient-text">
-        {bestStreak} Consecutive Blockbusters
-      </span>
-    </>
+   <>
+  <span className="streak-number best-gradient-text">
+    {bestStreak}
+  </span>
+
+  <span className="streak-label">
+    Consecutive Blockbusters
+  </span>
+</>
   )}
             </h3>
 
